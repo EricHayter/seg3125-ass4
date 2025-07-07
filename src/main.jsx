1518b22ba675be8467bd9d410e92e316ae66fca3
Eric <hayter.eric@gmail.com>
2025-07-06 20:23:10 -0400

    Fix basename for react router

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/seg3125-ass4/">
       <App />
     </BrowserRouter>
   </StrictMode>
